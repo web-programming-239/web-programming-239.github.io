fb7839e607705b26d48788a0606be0d676045087
matvi <matvik.gurov@gmail.com>
2024-09-07 17:34:52 +0300

    undo fix

--- src/App.js
+++ src/App.js
@@ -27,47 +27,46 @@ function App() {
                         <Col className={"text-center"}>
                             <p className={"mb-2 text-muted"}>Беседа в телеграм:</p>
                             <a href={"http://t.me/+fNRp_Y1MoXE3MjUy"}>
-                            <ArrowIcon style={{height: "8rem", width: "8rem"}}/></a>
+                                <ArrowIcon style={{height: "8rem", width: "8rem"}}/></a>
                             <p className={"mb-2 text-muted"}>Начало 19 сентября</p>
                         </Col>
                     </Row>
                 </Container>
 
-            {/*    <Container><*/}
-            {/*        h2 className={"mb-4 text-start"}>Как построен курс?</h2>*/}
-            {/*        <BottomTabs></BottomTabs>*/}
-            {/*    </Container>*/}
-            {/*    <Container className={"m-1 mt-5 mb-5 pt-3 pb-3 rounded-4"}>*/}
-            {/*        <h2 className={"mb-4 text-start"}>Кто будет вести курс?</h2>*/}
-            {/*        <Row className={"m-auto"} style={{maxWidth: "70rem"}}>*/}
-            {/*            <TeacherCard*/}
-            {/*                name={["Матвей","Гуров"]}*/}
-            {/*                subtitle={"15-5"}*/}
-            {/*                description={["Backend-разработчик в Yandex","КТ ИТМО"]}*/}
-            {/*                links={[*/}
-            {/*                    {title: "Telegram", link: "https://t.me/obomba", icon: TelegramIcon},*/}
-            {/*                    {title: "Github", link: "https://github.com/matvik22000", icon: GItIcon}*/}
-            {/*                ]}*/}
-            {/*            />*/}
-            {/*            <TeacherCard*/}
-            {/*                name={["Александр", "Крестьянинов"]}*/}
-            {/*                subtitle={"15-5"}*/}
-            {/*                description={["Backend-разработчик в Ditar", "ПМИ СПбГЭУ"]}*/}
-            {/*                links={[*/}
-            {/*                    {title: "Telegram", link: "https://t.me/AleksanderRefresher", icon: TelegramIcon},*/}
-            {/*                    {title: "Github", link: "https://github.com/hellagod", icon: GItIcon}*/}
-            {/*                ]}*/}
-            {/*            />*/}
-            {/*            <TeacherCard*/}
-            {/*                name={["Эльвира","Минько"]}*/}
-            {/*                subtitle={"15-6 "}*/}
-            {/*                description={["Frontend-разработчик в YADRO","КТ ИТМО"]}*/}
-            {/*                links={[*/}
-            {/*                    {title: "Telegram", link: "https://t.me/elvmin", icon: TelegramIcon},*/}
-            {/*                ]}*/}
-            {/*            />*/}
-            {/*        </Row>*/}
-            {/*    </Container>*/}
+                <Container><h2 className={"mb-4 text-start"}>Как построен курс?</h2>
+                    <BottomTabs></BottomTabs>
+                </Container>
+                <Container className={"m-1 mt-5 mb-5 pt-3 pb-3 rounded-4"}>
+                    <h2 className={"mb-4 text-start"}>Кто будет вести курс?</h2>
+                    <Row className={"m-auto"} style={{maxWidth: "70rem"}}>
+                        <TeacherCard
+                            name={["Матвей", "Гуров"]}
+                            subtitle={"15-5"}
+                            description={["Backend-разработчик в Yandex", "КТ ИТМО"]}
+                            links={[
+                                {title: "Telegram", link: "https://t.me/obomba", icon: TelegramIcon},
+                                {title: "Github", link: "https://github.com/matvik22000", icon: GItIcon}
+                            ]}
+                        />
+                        <TeacherCard
+                            name={["Александр", "Крестьянинов"]}
+                            subtitle={"15-5"}
+                            description={["Backend-разработчик в Ditar", "ПМИ СПбГЭУ"]}
+                            links={[
+                                {title: "Telegram", link: "https://t.me/AleksanderRefresher", icon: TelegramIcon},
+                                {title: "Github", link: "https://github.com/hellagod", icon: GItIcon}
+                            ]}
+                        />
+                        <TeacherCard
+                            name={["Эльвира", "Минько"]}
+                            subtitle={"15-6 "}
+                            description={["Frontend-разработчик в YADRO", "КТ ИТМО"]}
+                            links={[
+                                {title: "Telegram", link: "https://t.me/elvmin", icon: TelegramIcon},
+                            ]}
+                        />
+                    </Row>
+                </Container>
             </Container>
         </div>
     );
